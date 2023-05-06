cebe35fbc364b4f7061848cb311b26f4f72aaa41
Kesha005 <backendtm005@gmail.com>
2023-05-06 06:10:14 -0700

    first_commit

---
+++ first.js
@@ -0,0 +1,19 @@
+
+
+console.log("Hello world");
+
+var x = 3
+var y = 4
+
+
+let z = x + y
+
+if(x < y){
+    console.log("x bigger than y")
+}
+
+else {
+    console.log("y bigger than x")
+}
+console.log(z)
+
